e47e8a0cf271fef092d8da94e28e85561de020b4
andiigela <andiigela@gmail.com>
2024-01-11 18:43:04 +0100

    feature/Post-Management: Separated interviews in another controller

--- JobApp/src/main/java/com/ubt/andi/jobapp/controllers/JobsController.java
+++ JobApp/src/main/java/com/ubt/andi/jobapp/controllers/JobsController.java
@@ -139,51 +139,51 @@ public class JobsController {
         applicationService.editApplication(application);
         return "redirect:/job/"+jobId+"/applicants";
     }
-    @GetMapping("/job/{jobId}/applicants/{applicantId}/interview/create")
-    public String getInterviewCreate(@PathVariable("jobId") Long jobId,@PathVariable("applicantId") Long applicantId,Model model){
-        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
-        Profile profile = user.getProfile();
-        model.addAttribute("interview", new Interview());
-        model.addAttribute("profile", profile);
-        model.addAttribute("jobId", jobId);
-        model.addAttribute("applicantId", applicantId);
-        return "create-interview";
-    }
-    @PostMapping("/job/{jobId}/applicants/{applicantId}/interview/create")
-    public String createInterview(@PathVariable("jobId") Long jobId,@PathVariable("applicantId") Long applicantId,
-                                  @ModelAttribute("interview") Interview interview){
-        if(interview.getDateTime().isBefore(LocalDateTime.now())){
-            return "redirect:/job/" + jobId + "/applicants/" + applicantId + "/interview/create";
-        }
-        Job job = jobService.getJobById(jobId);
-        Profile applicantProfile = profileService.getProfileById(applicantId);
-        interviewService.createInterview(interview,job,applicantProfile);
-        notificationService.sendInterviewNotification(applicantProfile,job,interview);
-        return "redirect:/job/"+jobId+"/applicants";
-    }
-    @GetMapping("/interview/{id}")
-    public String viewInterviewByApplicant(@PathVariable("id") Long id, Model model){
-        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
-        Profile profile = user.getProfile();
-        Interview interview = interviewService.findInterviewById(id);
-        if(interview.getProfile().getId() != profile.getId()) return "redirect:/";
-        model.addAttribute("interview", interview);
-        model.addAttribute("profile", profile);
-        return "view-interview";
-    }
-    @GetMapping("/interviews")
-    public String getInterviewsPage(@RequestParam(value = "page",defaultValue = "0") String page,Model model){
-        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
-        Profile profile = user.getProfile();
-        if(profile == null) return "redirect:/profile/view/"+user.getUsername();
-        int pageNumber = Integer.parseInt(page);
-        if(pageNumber > 0){
-            pageNumber-=1;
-        }
-        Pageable pageable = PageRequest.of(pageNumber,PAGE_SIZE);
-        Page<Interview> interviews = interviewService.findInterviewsByUser(pageable);
-        model.addAttribute("profile",profile);
-        model.addAttribute("interviews",interviews);
-        return "my-interviews";
-    }
+//    @GetMapping("/job/{jobId}/applicants/{applicantId}/interview/create")
+//    public String getInterviewCreate(@PathVariable("jobId") Long jobId,@PathVariable("applicantId") Long applicantId,Model model){
+//        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+//        Profile profile = user.getProfile();
+//        model.addAttribute("interview", new Interview());
+//        model.addAttribute("profile", profile);
+//        model.addAttribute("jobId", jobId);
+//        model.addAttribute("applicantId", applicantId);
+//        return "create-interview";
+//    }
+//    @PostMapping("/job/{jobId}/applicants/{applicantId}/interview/create")
+//    public String createInterview(@PathVariable("jobId") Long jobId,@PathVariable("applicantId") Long applicantId,
+//                                  @ModelAttribute("interview") Interview interview){
+//        if(interview.getDateTime().isBefore(LocalDateTime.now())){
+//            return "redirect:/job/" + jobId + "/applicants/" + applicantId + "/interview/create";
+//        }
+//        Job job = jobService.getJobById(jobId);
+//        Profile applicantProfile = profileService.getProfileById(applicantId);
+//        interviewService.createInterview(interview,job,applicantProfile);
+//        notificationService.sendInterviewNotification(applicantProfile,job,interview);
+//        return "redirect:/job/"+jobId+"/applicants";
+//    }
+//    @GetMapping("/interview/{id}")
+//    public String viewInterviewByApplicant(@PathVariable("id") Long id, Model model){
+//        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+//        Profile profile = user.getProfile();
+//        Interview interview = interviewService.findInterviewById(id);
+//        if(interview.getProfile().getId() != profile.getId()) return "redirect:/";
+//        model.addAttribute("interview", interview);
+//        model.addAttribute("profile", profile);
+//        return "view-interview";
+//    }
+//    @GetMapping("/interviews")
+//    public String getInterviewsPage(@RequestParam(value = "page",defaultValue = "0") String page,Model model){
+//        AppUser user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+//        Profile profile = user.getProfile();
+//        if(profile == null) return "redirect:/profile/view/"+user.getUsername();
+//        int pageNumber = Integer.parseInt(page);
+//        if(pageNumber > 0){
+//            pageNumber-=1;
+//        }
+//        Pageable pageable = PageRequest.of(pageNumber,PAGE_SIZE);
+//        Page<Interview> interviews = interviewService.findInterviewsByUser(pageable);
+//        model.addAttribute("profile",profile);
+//        model.addAttribute("interviews",interviews);
+//        return "my-interviews";
+//    }
 }
